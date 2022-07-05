dad711efb3b82d6ca252f89c13a5bc75f27b951f
abdikeem <abdihakimabdirahman21@gmail.com>
2022-07-05 22:48:25 +0300

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from tkinter import E
-from web3 import web3, EthereumTesterProvider
-w3 = web3(EthereumTesterProvider())
+import json
+from web3 import Web3
 
-w3.isConnected()
+infura_url = "https://mainnet.infura.io/v3/e9f149bc1a424e689eefdd70d379e11c"
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+print(web3.isConnected())
